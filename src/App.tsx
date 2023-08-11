8dfea094d1372a93579f9e0b2f8674a9d2143f68
kevindmorris <kdm0030@mix.wvu.edu>
2023-08-10 22:30:22 -0400

    chart setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,91 @@
+import { Container, Paper, TextField, useTheme } from "@mui/material";
+import React from "react";
+import Chart from "./Chart";
+
 export default function App() {
-  return <div>Simplifi</div>;
+  const theme = useTheme();
+
+  const [initialDeposit, setInitialDeposit] = React.useState<number>(20000);
+  const [yearlyContribution, setYearlyContribution] = React.useState<number>(1000);
+  const [yearsOfGrowth, setYearsOfGrowth] = React.useState<number>(10);
+  const [rateOfReturn, setRateOfReturn] = React.useState<number>(6);
+
+  return (
+    <Container
+      style={{
+        width: "100vw",
+        height: "100vh",
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+        display: "flex",
+        gap: theme.spacing(6),
+      }}
+    >
+      <Paper
+        variant="outlined"
+        sx={{
+          flex: 1,
+          height: "100%",
+          padding: theme.spacing(3),
+          display: "flex",
+          flexDirection: "column",
+          gap: theme.spacing(3),
+        }}
+      >
+        <TextField
+          fullWidth
+          label="Initial Deposit"
+          type="number"
+          value={initialDeposit}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            let newValue = parseFloat(event.target.value);
+            setInitialDeposit(newValue);
+          }}
+        />
+        <TextField
+          fullWidth
+          label="Yearly Contribution"
+          type="number"
+          value={yearlyContribution}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            let newValue = parseFloat(event.target.value);
+            setYearlyContribution(newValue);
+          }}
+        />
+        <TextField
+          fullWidth
+          label="Years of Growth"
+          type="number"
+          value={yearsOfGrowth}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            let newValue = parseInt(event.target.value);
+            setYearsOfGrowth(newValue);
+          }}
+        />
+        <TextField
+          fullWidth
+          label="Yearly Rate of Return"
+          type="number"
+          value={rateOfReturn}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            let newValue = parseFloat(event.target.value);
+            setRateOfReturn(newValue);
+          }}
+        />
+      </Paper>
+      <Paper variant="outlined" sx={{ flex: 2, height: "100%" }}>
+        {initialDeposit &&
+        yearlyContribution &&
+        rateOfReturn &&
+        yearsOfGrowth ? (
+          <Chart
+            a={initialDeposit}
+            p={yearlyContribution}
+            r={rateOfReturn / 100}
+            t={yearsOfGrowth}
+          />
+        ) : null}
+      </Paper>
+    </Container>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>
+);
